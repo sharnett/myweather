154d8629d6b627b3290adf3a143ff3033637173e
Sean Harnett <srharnett@gmail.com>
2012-02-10 14:49:14 -0500

    nothing to see here

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'myweather.urls'
 
 TEMPLATE_DIRS = (
-    "templates/",
+    "/home/sean/myweather/templates/",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
