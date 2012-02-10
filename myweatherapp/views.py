adbcd48c3a06e8a82dd05b773f9b675dca320d4d
Sean Harnett <srharnett@gmail.com>
2012-02-10 14:53:51 -0500

    added better default behavior

--- myweatherapp/views.py
+++ myweatherapp/views.py
@@ -8,11 +8,15 @@ def home(request):
 def WeatherView(request):
     ds = ''
     params = dict((k, v) for k, v in request.GET.iteritems())
-    if not params or not params['zip_code'] or not params['num_hours']:
-        pass
+    if not params or not params['zip_code']:
+        zip_code = 10025
     else:
         zip_code = int(params['zip_code']) 
+    if not params or not params['num_hours']:
+        num_hours = 8
+    else:
         num_hours = int(params['num_hours'])
-        w = weather_for_zip(zip_code)
-        ds = get_shit_i_care_about(w, num_hours)
-    return render_to_response('weather_form.html', {'data_string': ds})
+    w = weather_for_zip(zip_code)
+    ds = get_shit_i_care_about(w, num_hours)
+    return render_to_response('weather_form.html', \
+            {'data_string': ds, 'zip_code': zip_code, 'num_hours': num_hours})
