29f444226f90b14f41fe47bf2084d3197afeb6ba
Sean Harnett <srharnett@gmail.com>
2012-03-03 18:41:23 -0500

    icons are a go

--- get_json.py
+++ get_json.py
@@ -1,7 +1,5 @@
 from urllib2 import urlopen
 from json import load
-import numpy as np
-from scipy.interpolate import interp1d
 
 KEY = open('/Users/srharnett/Dropbox/hacking/myweather/key.txt', 'r').read().strip()
 feature = 'hourly7day'
@@ -11,29 +9,23 @@ def weather_for_zip(zip_code):
     url = url_base % (KEY, feature, zip_code)
     return load(urlopen(url))
 
-def interpolate(time, temp, feels_like, pop):
-    t = np.linspace(time[0], time[-1])
-    x1 = interp1d(time, temp, kind='cubic')
-    x2 = interp1d(time, feels_like, kind='cubic')
-    x3 = interp1d(time, pop, kind='cubic')
-    return t,x1(t),x2(t),x3(t)
-
 def get_shit_i_care_about(w, num_hours):
     if not 'hourly_forecast' in w or not w['hourly_forecast']:
         return ''
-    rows, time, temp, feels_like, pop = [],[],[],[],[]
+    rows, icon, time, temp, feels_like, pop = [],[],[],[],[],[]
     for i,f in enumerate(w['hourly_forecast'][0:num_hours]):
+        icon += [f['icon_url']]
         time += [int(f['FCTTIME']['epoch'])*1000]  # date and time
         temp += [f['temp']['english']]             # temperature
         feels_like += [f['feelslike']['english']]  # temperature it feels like
         pop += [f['pop']]                          # percentage chance precipitation
-    if num_hours < 100:
-        time,temp,feels_like,pop = interpolate(time, temp, feels_like, pop)
+    icon_pos = 100
     for i in range(len(time)):
-        row = "[new Date(%d), %s, %s, %s]" % (time[i], temp[i], feels_like[i], pop[i])
+        row = "{date: new Date(%d),\n icon: '%s', icon_pos: %s, temp: %s, pop: %s, feel: %s}" % \
+                (time[i], icon[i], icon_pos, temp[i], pop[i], feels_like[i])
         rows.append(row)
-    return ",\n".join(rows)
+    return "[" + ",\n".join(rows) + "]"
         
 if __name__ == "__main__":
     w = weather_for_zip(10025)
-    print get_shit_i_care_about(w,1000)
+    print get_shit_i_care_about(w,10)
---
+++ myweatherapp/static/weather.js
@@ -0,0 +1,92 @@
+function weather() {
+    var chart = new AmCharts.AmSerialChart();
+    chart.dataProvider = lineChartData;
+    chart.pathToImages = "static/";
+    chart.categoryField = "date";
+    chart.marginTop = 40;
+    chart.addTitle(num_hours.toString() + ' hour forecast for zip ' + zip_code.toString());
+
+    // AXES
+    // category                
+    var categoryAxis = chart.categoryAxis;
+    categoryAxis.parseDates = true; 
+    categoryAxis.minPeriod = "hh"; 
+    categoryAxis.dashLength = 4;
+    categoryAxis.gridAlpha = .15;
+    categoryAxis.dateFormats = [{period: "DD", format: "EEE M/D"}, {period: "hh", format: "LA"}]
+
+    // temperature value axis
+    var tempAxis = new AmCharts.ValueAxis();
+    tempAxis.dashLength = 4;
+    tempAxis.gridAlpha = 0;
+    tempAxis.unit = " F";
+    chart.addValueAxis(tempAxis);
+
+    // pop value axis
+    var popAxis = new AmCharts.ValueAxis();
+    popAxis.position = "right";
+    popAxis.dashLength = 4;
+    popAxis.gridAlpha = 0;
+    popAxis.minimum = 0;
+    popAxis.maximum = 100;
+    popAxis.unit = "%";
+    chart.addValueAxis(popAxis);
+
+    // GRAPH
+    // icons graph
+    var icons = new AmCharts.AmGraph();
+    icons.position = "right";
+    icons.type = "line";
+    icons.valueAxis = popAxis;
+    icons.valueField = "icon_pos";
+    icons.bulletSize = 42; 
+    icons.customBulletField = "icon"; 
+    icons.lineThickness = 0;
+    icons.visibleInLegend = false;
+    icons.showBalloon = false;
+    chart.addGraph(icons);
+
+    // feels like graph
+    var feel = new AmCharts.AmGraph();
+    feel.valueAxis = tempAxis;
+    feel.type = "smoothedLine";
+    feel.valueField = "feel";
+    feel.title = "Feels like";
+    feel.lineColor = 'green';
+    feel.balloonText = "[[value]] F";
+    chart.addGraph(feel);
+
+    // temp graph
+    var temp = new AmCharts.AmGraph();
+    temp.valueAxis = tempAxis;
+    temp.type = "smoothedLine";
+    temp.valueField = "temp";
+    temp.title = "Temperature";
+    temp.lineColor = 'red';
+    temp.balloonText = "[[value]] F";
+    chart.addGraph(temp);
+
+    // pop graph
+    var pop = new AmCharts.AmGraph();
+    pop.valueAxis = popAxis;
+    pop.type = "smoothedLine";
+    pop.valueField = "pop";
+    pop.title = "% chance of rain";
+    pop.lineColor = 'blue';
+    pop.balloonText = "[[value]]%";
+    chart.addGraph(pop);
+
+    // CURSOR
+    var chartCursor = new AmCharts.ChartCursor();
+    chartCursor.categoryBalloonDateFormat = "M/D LA";
+    chartCursor.cursorColor = "orange";
+    chart.addChartCursor(chartCursor);
+
+    // LEGEND
+    var legend = new AmCharts.AmLegend();
+    legend.position = "top";
+    chart.addLegend(legend);
+
+    // WRITE
+    chart.write("chart_div");
+};
