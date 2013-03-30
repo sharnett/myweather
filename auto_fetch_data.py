6b2f40175a82805e0d8cef3835d99b6068a91d87
sean harnett <srharnett@gmail.com>
2013-03-31 03:36:25 +0400

    bug fix

--- auto_fetch_data.py
+++ auto_fetch_data.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 from get_json import weather_for_zip
+from time import asctime
+from os.path import dirname, abspath
 
 if __name__ == '__main__':
     zipcodes = {'10025', '10027', '02139', '94110'}
     for z in zipcodes:
-        print z
         weather_for_zip(z)
+    directory = dirname(abspath(__file__))
+    open(directory + 'timelog.txt', 'a').write(asctime()+'\n')
