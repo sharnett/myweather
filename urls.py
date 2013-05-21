086d058fe3158a9aab6abb919a7d5c24488cdb08
Sean Harnett <srharnett@gmail.com>
2013-05-21 17:09:58 -0400

    removed django crap, added fabfile

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-from myweatherapp.views import WeatherView, LunchView
-
-urlpatterns = patterns('',
-    url(r'^$', WeatherView),
-    url(r'^lunch/$', LunchView.as_view()),
-)
