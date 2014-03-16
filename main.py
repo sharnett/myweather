92b290102001f8390539e9b4349254ff02ddc88d
Sean Harnett <srharnett@gmail.com>
2014-03-16 16:24:45 -0400

    added logging

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import flask
+import logging
+import logging.handlers
 from flask import render_template, request, session
 from get_json import weather_for_zip, geolookup, limit, get_location
 from re import match
@@ -22,35 +24,57 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///db.db'
 app.config.from_object(__name__)
 db.init_app(app)
 
+log = logging.getLogger('seanweather')
+log.setLevel(logging.DEBUG)
+fh = logging.handlers.RotatingFileHandler('seanweather.log', maxBytes=10000000)
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+log.addHandler(fh)
+
 def main():
     app.run(host='0.0.0.0')
 
 @app.route('/', methods=['GET'])
 def home():
+    log.info('STARTING')
     prev_zip = session.get('zipcode', '10027')
     try:
         zipcode = request.args.get('zipcode', prev_zip)
         assert(match(r'^\d{5}$', zipcode))
+        log.info('got %s from the request or session' % zipcode)
     except:
+        log.info('%s doesnt look like a zipcode, trying to parse' % zipcode)
         zipcode = get_location(zipcode)
         if zipcode is None:
             zipcode = '10027'
+            log.info('couldnt parse location, using %s' % zipcode)
+        else:
+            log.info('%s parsed' % zipcode)
     try:
         num_hours = int(request.args.get('num_hours', session.get('num_hours', 12)))
     except:
         num_hours = 12
     location = Location.query.get(zipcode)
     if not location:
+        log.info('%s wasnt in the cache, looking up geo information' % zipcode)
         location = Location(zipcode, geolookup(zipcode))
+    else:
+        log.info('%s was in the cache, reusing geoinformation' % zipcode)
+    log.info('%s is %s' % (zipcode, location.city))
     if (datetime.now()-location.last_updated).seconds > 2700:
+        log.info('looking up the weather for %s' % zipcode)
         location.cache = dumps(weather_for_zip(zipcode))
         location.last_updated = datetime.now()
+    else:
+        log.info('weather for %s was recently cached, reusing' % zipcode)
     ds = limit(loads(location.cache), num_hours)
     location = db.session.merge(location)
     db.session.add(Lookup(location))
     db.session.commit()
     session['zipcode'] = zipcode
     session['num_hours'] = num_hours
+    log.info('FINISHED with %s' % zipcode)
     return render_template('weather_form.html', data_string=ds, city=location.city, 
             zipcode=zipcode, num_hours=num_hours)
 
