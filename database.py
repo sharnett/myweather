28e9821f86a2ecd752dfc8933151e1f907eab5b6
Sean Harnett <srharnett@gmail.com>
2016-04-12 19:43:27 -0400

    massive changes to data model

--- database.py
+++ database.py
@@ -4,35 +4,29 @@ from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Location(db.Model):
-    zipcode = db.Column(db.String, primary_key=True)
-    city = db.Column(db.String)
+    zmw = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String)
+    url = db.Column(db.String)
     cache = db.Column(db.String)
     last_updated = db.Column(db.DateTime)
-    def __init__(self, zipcode, city='', cache=''):
-        self.zipcode = zipcode
-        self.city = city
+    def __init__(self, zmw, name='', url='', cache=''):
+        self.zmw = zmw
+        self.name = name
+        self.url = url
         self.cache = cache
         self.last_updated = datetime.now() if cache else datetime.fromtimestamp(0)
     def __repr__(self):
-        return '<Zipcode %r, %r>' % (self.zipcode, self.city)
+        return '<Location %r, %r>' % (self.zmw, self.name)
 
 class Lookup(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime)
-    zipcode = db.Column(db.String, db.ForeignKey('location.zipcode'))
+    user_input = db.Column(db.String, index=True)
+    name = db.Column(db.String, db.ForeignKey('location.name'))
     location = db.relationship('Location', backref=db.backref('lookups', lazy='dynamic'))
-    def __init__(self, location):
+    def __init__(self, user_input, location):
         self.date = datetime.now()
+        self.user_input = user_input
         self.location = location
     def __repr__(self):
-        return '<Lookup %r, %r>' % (self.zipcode, self.date)
-
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime)
-    text = db.Column(db.String)
-    def __init__(self, text):
-        self.date = datetime.now()
-        self.text = text
-    def __repr__(self):
-        return '<Comment %r, %r>' % (self.date, self.text)
+        return '<Lookup %r, %r, %r>' % (self.user_input, self.name, self.date)
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from get_json import weather_for_url, parse_user_input, limit_hours
 from re import match
 from traceback import format_exc
 from os import environ
-from database import db, Location, Lookup, Comment
+from database import db, Location, Lookup
 from datetime import datetime
 from json import loads, dumps
 
@@ -35,49 +35,60 @@ log.addHandler(fh)
 def main():
     app.run(host='0.0.0.0')
 
+
+def get_location(user_input):
+    ''' first check cache. if missing or too old, use autocomplete API '''
+    last_lookup = Lookup.query.filter_by(user_input=user_input).order_by(Lookup.date.desc()).first()
+    log.info('db result for location: ' + str(last_lookup))
+    if last_lookup is not None and (datetime.now()-last_lookup.date).seconds < 604800:
+        log.info('got location info from the cache')
+        return last_lookup.location, user_input
+    try:
+        url, location_name, zmw = parse_user_input(user_input)
+        log.info('got location info from autocomplete API')
+        log.info('%s -> %s, %s, %s' % (user_input, url, location_name, zmw))
+        return Location(zmw, url=url, name=location_name), user_input
+    except IndexError, KeyError:
+        flask.flash('seanweather didnt like that, please try another city or zipcode')
+        log.warning('failed to parse: %s. Using 10027' % user_input)
+        last_default = Lookup.query.filter_by(user_input='10027').order_by(Lookup.date.desc()).first()
+        if last_default is not None:
+            location = last_default.location
+        else:
+            url, location_name, zmw = '/q/zmw:10027.1.99999', '10027 - New York, NY', '10027.1.99999'
+            location = Location(zmw, url=url, name=location_name)
+        return location, '10027'
+
+
 @app.route('/', methods=['GET'])
 def home():
     log.info('STARTING')
-    prev_url = session.get('url', '/q/zmw:10027')
-    user_input = request.args.get('user_input', '10025')
-    try:
-        url, location_name, zmw = parse_user_input(user_input)
-    except IndexError, KeyError:
-        flask.flash('seanweather didnt like that, please try another city or zipcode')
-        return render_template('weather_form.html', data_string='', city='',
-            zmw='', num_hours=12)
-    else:
-        location = Location(zmw, location_name)
-        log.info('succesfully parsed. %s -> %s, %s, %s' % (user_input, url, location_name, zmw))
-        log.info('%s' % location)
+    user_input = request.args.get('user_input',
+                                  session.get('user_input', '10027'))
+    location, user_input = get_location(user_input)
+    log.info('%s' % location)
     try:
         num_hours = int(request.args.get('num_hours', session.get('num_hours', 12)))
     except:
         flask.flash('seanweather didnt like the number of hours, using 12')
         num_hours = 12
-    if location is None:
-        location = Location.query.get(zipcode)
-    if location is None:
-        log.info('%s wasnt in the cache, looking up geo information' % zipcode)
-        location = Location(zipcode, geolookup(zipcode))
-    else:
-        log.info('%s was in the cache, reusing geoinformation' % url)
     if (datetime.now()-location.last_updated).seconds > 2700 or len(location.cache) == 0:
-        log.info('looking up the weather for %s' % location_name)
-        location.cache = dumps(weather_for_url(url))
+        log.info('using weather API for %s' % location.zmw)
+        location.cache = dumps(weather_for_url(location.url))
         location.last_updated = datetime.now()
     else:
-        log.info('weather for %s was recently cached, reusing' % zipcode)
+        log.info('weather for %s was recently cached, reusing' % location.zmw)
     ds = limit_hours(loads(location.cache), num_hours)
     location = db.session.merge(location)
-    db.session.add(Lookup(location))
+    db.session.add(Lookup(user_input, location))
     db.session.commit()
-    session['url'] = url
+    session['user_input'] = user_input
     session['num_hours'] = num_hours
     session.permanent = True
     log.info('FINISHED with %s' % user_input)
-    return render_template('weather_form.html', data_string=ds, city=location.city, 
-            zmw=zmw, num_hours=num_hours)
+    return render_template('weather_form.html', data_string=ds, city=location.name, 
+            user_input=user_input, num_hours=num_hours)
+
 
 @app.route('/comment', methods=['POST'])
 def comment():
@@ -87,6 +98,7 @@ def comment():
         db.session.commit()
         flask.flash('We appreciate your feedback! :)')
     return flask.redirect(flask.url_for('home'))
+
 
 @app.route('/discuss')
 def discuss():
