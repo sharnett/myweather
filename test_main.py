19add16335de2df03daa87895b84ac311bfa5512
Sean Harnett <srharnett@gmail.com>
2017-06-23 22:40:18 +0100

    forgot to add test_main.py at some point

---
+++ test_main.py
@@ -0,0 +1,20 @@
+import main as seanweather
+
+def test_parse_temps():
+    weather_data = [{'temp': '83', 'temp_c': '28'},
+                    {'temp': '81', 'temp_c': '27'},
+                    {'temp': '85', 'temp_c': '29'}]
+    weather_data *= 10
+    # The below two data points are outside the default 24-hour period, so they
+    # should not be included in the max/min calculation
+    weather_data.append({'temp': '100', 'temp_c': '100'})
+    weather_data.append({'temp': '0', 'temp_c': '0'})
+
+    f_current, f_max, f_min = seanweather.parse_temps(weather_data)
+    assert f_current == 83
+    assert f_max == 85
+    assert f_min == 81
+    c_current, c_max, c_min = seanweather.parse_temps(weather_data, units='C')
+    assert c_current == 28
+    assert c_max == 29
+    assert c_min == 27
