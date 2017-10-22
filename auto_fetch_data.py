f08d4b7a72b9c05a8f2fcc6d41ec321fc4663a33
Sean Harnett <srharnett@gmail.com>
2017-10-22 18:36:28 +0100

    fix auto_fetch api key bug

--- auto_fetch_data.py
+++ auto_fetch_data.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
-from get_json import weather_for_url
+import os
+from wunderground import weather_for_url
 from datetime import datetime
 from json import dumps
 from sqlite3 import connect
 from os.path import dirname, abspath
+
+API_KEY = os.environ.get('WUNDERGROUND_KEY', 'development')
 
 if __name__ == '__main__':
     directory = dirname(abspath(__file__))
@@ -12,7 +15,7 @@ if __name__ == '__main__':
     zipcodes = {'10003', '12180', '11105', '11215'}
     for z in zipcodes:
         url = '/q/zmw:%s.1.99999' % z
-        cache = dumps(weather_for_url(url))
+        cache = dumps(weather_for_url(url, API_KEY))
         last_updated = datetime.now()
         c.execute('update location set cache=?, last_updated=? where url=?',
                   (cache, last_updated, url))
