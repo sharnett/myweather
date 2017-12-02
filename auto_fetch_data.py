8ef9ad160eb28ac18e08af3a73e92986845a4c05
Sean Harnett <srharnett@gmail.com>
2017-12-02 15:29:11 +0000

    use SQLAlchemy for auto_fetch_data

--- auto_fetch_data.py
+++ auto_fetch_data.py
@@ -1,24 +1,21 @@
-#!/usr/bin/python
-import os
-from wunderground import weather_for_url
-from datetime import datetime
-from json import dumps
-from sqlite3 import connect
+import sqlalchemy
+import json
+from wunderground import weather_for_url, autocomplete_user_input
 from os.path import dirname, abspath
+from database import Location
 
 API_KEY = os.environ.get('WUNDERGROUND_KEY', 'development')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.db'
+
+def get_db_session():
+    engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+    return sqlalchemy.orm.sessionmaker(bind=engine)()
 
 if __name__ == '__main__':
-    directory = dirname(abspath(__file__))
-    conn = connect(directory + '/db.db')
-    c = conn.cursor()
+    session = get_db_session()
     zipcodes = {'10003', '12180', '11105', '11215'}
     for z in zipcodes:
-        url = '/q/zmw:%s.1.99999' % z
-        cache = dumps(weather_for_url(url, API_KEY))
-        last_updated = datetime.now()
-        c.execute('update location set cache=?, last_updated=? where url=?',
-                  (cache, last_updated, url))
-
-    conn.commit()
-    conn.close()
+        url, name = autocomplete_user_input(z)
+        cache = json.dumps(weather_for_url(url, API_KEY))
+        session.merge(Location(url, name=name, cache=cache))
+    session.commit()
