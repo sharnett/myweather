4c081d8a67775cf512f898be335f658b755b7f0b
Sean Harnett <srharnett@gmail.com>
2018-02-21 22:31:04 +0000

    ordered wunderground functions more sensibly

--- wunderground.py
+++ wunderground.py
@@ -10,6 +10,40 @@ logging.basicConfig()
 log = logging.getLogger(__name__)
 
 
+def autocomplete_user_input(user_input, opener=urlopen):
+    ''' Takes contents of 'enter zip code or city' field and gives it to the
+    wunderground autocomplete API. Returns the url and name of the top
+    result, e.g.
+
+    '/q/zmw:10027.1.99999', '10027 - New York, NY',
+    '''
+    url = ('http://autocomplete.wunderground.com/aq?query=%s'
+           % urllib.quote(user_input.encode('utf-8')))
+    response = json.load(opener(url))
+    # will raise an IndexError if RESULTS is empty
+    top_result = response['RESULTS'][0]
+    return top_result['l'], top_result['name']
+
+
+def weather_for_url(url, api_key):
+    return _parse_json(_json_for_url(url, api_key))
+
+
+def _json_for_url(location_url, api_key, opener=urlopen):
+    if api_key == 'development':
+        logging.error('You need to set WUNDERGROUND_KEY in your environment')
+        return {}
+    base_url = (
+        'http://api.wunderground.com/api/{key}/hourly10day{location_url}.json')
+    url = base_url.format(key=api_key, location_url=location_url)
+    for i in range(3):
+        try:
+            return json.load(opener(url))
+        except URLError:
+            time.sleep(i)
+    raise URLError('urlopen timeout max retries')
+
+
 def _parse_json(json_data):
     ''' Parse json returned from wunderground API into list of dicts with
         only the data we care about.
@@ -29,40 +63,6 @@ def _parse_json(json_data):
             feel=row['feelslike']['english'],  # temperature it feels like
             feel_c=row['feelslike']['metric'])
     return [get_dict(row) for row in json_data['hourly_forecast']]
-
-
-def autocomplete_user_input(user_input, opener=urlopen):
-    ''' Takes contents of 'enter zip code or city' field and gives it to the
-    wunderground autocomplete API. Returns the url and name of the top
-    result, e.g.
-
-    '/q/zmw:10027.1.99999', '10027 - New York, NY',
-    '''
-    url = ('http://autocomplete.wunderground.com/aq?query=%s'
-           % urllib.quote(user_input.encode('utf-8')))
-    response = json.load(opener(url))
-    # will raise an IndexError if RESULTS is empty
-    top_result = response['RESULTS'][0]
-    return top_result['l'], top_result['name']
-
-
-def _json_for_url(location_url, api_key, opener=urlopen):
-    if api_key == 'development':
-        logging.error('You need to set WUNDERGROUND_KEY in your environment')
-        return {}
-    base_url = (
-        'http://api.wunderground.com/api/{key}/hourly10day{location_url}.json')
-    url = base_url.format(key=api_key, location_url=location_url)
-    for i in range(3):
-        try:
-            return json.load(opener(url))
-        except URLError:
-            time.sleep(i)
-    raise URLError('urlopen timeout max retries')
-
-
-def weather_for_url(url, api_key):
-    return _parse_json(_json_for_url(url, api_key))
 
 
 if __name__ == "__main__":
